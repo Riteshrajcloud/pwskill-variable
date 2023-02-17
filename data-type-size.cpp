57b2a13628423117fbf42336886c1bf6827fb4e2
Ritesh Raj <76565131+Riteshrajcloud@users.noreply.github.com>
2023-02-17 13:41:06 +0530

    Create data-type-size.cpp

---
+++ data-type-size.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"size of fundamental data types:"<<endl;
+    cout<<"The size of char="<<sizeof(char)<<endl;
+    cout<<"The size of short="<<sizeof(short)<<endl;
+    cout<<"The size of int="<<sizeof(int)<<endl;
+    cout<<"The size of long="<<sizeof(long)<<endl;
+    cout<<"The size of long long="<<sizeof(long long)<<endl;
+    cout<<"The size of float="<<sizeof(float)<<endl;
+    cout<<"The size of double="<<sizeof(double)<<endl;
+    cout<<"The size of long double="<<sizeof(long double)<<endl;
+    cout<<"The size of bool="<<sizeof(bool)<<endl;
+}
