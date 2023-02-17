54e0f3e7afc34ff47daeba3c2cda9b5c6ee7220f
Ritesh Raj <76565131+Riteshrajcloud@users.noreply.github.com>
2023-02-17 13:45:44 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,temp;
+    cout<<"enter value of a and b=";
+    cin>>a>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"a="<<a<<" "<<"b="<<b;
+}
