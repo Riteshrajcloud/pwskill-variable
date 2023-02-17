ef54a7cade57ba19df8eef550653ac94bc022b8f
Ritesh Raj <76565131+Riteshrajcloud@users.noreply.github.com>
2023-02-17 13:28:15 +0530

    Create rectangle-area.cpp

---
+++ rectangle-area.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int l,b,area;
+    cout<<"Enter length of rectangle:";
+    cin>>l;
+    cout<<"Enter Breadth of rectangle:";
+    cin>>b;
+    area=l*b;
+    cout<<"area="<<area;
+}
